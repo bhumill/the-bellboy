319a1617f72bdd428ddfc3ebeb9afb219101c719
GurpreetKulvirSingh <gurpreetkaurjal9@gmail.com>
2021-02-17 16:00:54 -0500

    added

--- app/src/main/java/com/bhumil/thebellboy/MainActivity.java
+++ app/src/main/java/com/bhumil/thebellboy/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
