6c465519685e6702df0a84de4ad1e703e4b2a9a2
GurpreetKulvirSingh <gurpreetkaurjal9@gmail.com>
2021-02-24 15:44:14 -0500

    New Home page Design

--- app/src/main/java/com/bhumil/thebellboy/MainActivity.java
+++ app/src/main/java/com/bhumil/thebellboy/MainActivity.java
@@ -1,15 +1,10 @@
 package com.bhumil.thebellboy;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import com.google.firebase.auth.FirebaseAuth;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
